3a7bcd9b849e86b07a8f085da63fce5bf6f87e76
Samuel Soares <52789120+Samuel634@users.noreply.github.com>
2025-06-28 16:13:20 -0300

    Add files via upload

---
+++ 06.1-funcao_parametro.py
@@ -0,0 +1,10 @@
+def escrever(titulo):
+    print(titulo)
+
+print('-' * 30)
+escrever('Fábrica de Programadores')
+print('-' * 30)
+escrever('Turma de Sábado')
+print('-' * 30)
+escrever('Samuel Soares Lima')
+print('-' * 30)
---
+++ 06.5-funcao_random.py
@@ -0,0 +1,5 @@
+import random
+def sorteio(lista):
+    return random.choice(lista)
+
+print(sorteio(['Lavar o Banheiro', 'Lavar a Louça', 'Varrer o Quintal', 'Retirar o Lixo' ]))
